54f31bdc3ca5e1618122902fcea32a560f6a629c
T-Wesst <code.daddy17@gmail.com>
2020-11-25 22:01:18 -0800

    array types

---
+++ README.md
@@ -0,0 +1,9 @@
+# TypeScript
+
+## CORE TYPES
+
+- Number: all numbers, no differentiation between integers or floats
+- String: all text values
+- Boolean: only true or false values (no truthy-falsy)
+- Object: any JS object, more specific types of objects
+- Array: any javascript array, type can be flexible or strict (regarding element types)
--- app.ts
+++ app.ts
@@ -1,9 +1,19 @@
-const person: {
-  name: string;
-  age: number;
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+  const person = {
   name: 'tyroo',
-  age: 28
+  age: 28,
+  hobbies: ['gaming', 'skateboarding']
 }
 
+let favoriteActivity: string[];
+favoriteActivity = ['sports'];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies){
+  console.log(hobby.toUpperCase()) // TypeScript inference: hobby string
+  // console.log(hobby.map()) !! error
+}
