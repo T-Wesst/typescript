ca2e287cd8e6b8808d3118d99161ccd89e49aec3
T-Wesst <code.daddy17@gmail.com>
2020-12-14 20:57:24 -0800

    wrap up

---
+++ basics/objs-arrays-enums.ts
@@ -0,0 +1,44 @@
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   // tuple
+//   role: [number, string];
+// } = {
+//   name: 'tyroo',
+//   age: 28,
+//   hobbies: ['gaming', 'skateboarding'],
+//   role: [2, 'author']
+// }
+
+// defaults starts at 0, can modify to strings
+enum Role { ADMIN, READ_ONLY = 5, AUTHOR = 'Author'};
+
+const person = {
+  name: 'tyroo',
+  age: 28,
+  hobbies: ['gaming', 'skateboarding'],
+  role: Role.ADMIN
+}
+
+
+// person.role.push('admin'); not enforced by tuple
+// person.role[1] = 10;
+
+// tuple does not allow more than two elements
+// person.role = [0, 'admin', 'user'];
+
+// avoid any because takes away all advantages of TypeScript === vanilla JS
+let favoriteActivity: any[];
+favoriteActivity = ['sports'];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies){
+  console.log(hobby.toUpperCase()) // TypeScript inference: hobby string
+  // console.log(hobby.map()) !! error
+}
+
+if(person.role === Role.ADMIN){
+  console.log('admin');
+}
---
+++ basics/unknown-never.ts
@@ -0,0 +1,16 @@
+//  unkown types
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = 'max';
+if(typeof userInput === 'string') {
+ userName = userInput;
+}
+
+// never type
+function generateError(message: string, code: number): never {
+ throw {message, errorCode: code};
+}
+
+generateError('an error occured', 5000);
