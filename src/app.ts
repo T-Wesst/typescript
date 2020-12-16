d9e62a6f12a77d59cf52f7d835024db0aa877b21
T-Wesst <code.daddy17@gmail.com>
2020-12-15 20:39:27 -0800

    spread operator obj-arr

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,25 @@
+// const and let
 const userName = 'Max';
 let age = 28;
 age = 29;
 // defaults should be set from right
+// arrow functions
 const add = (a:number, b:number = 1) =>  a + b;
 // valid TS
 const printOutput: (a:number | string) => void = output => console.log(output);
 const printOutput1 = (output: string | number) => console.log(output);
-
 printOutput(add(5));
+
+// Spread Operator Arrays
+const hobbies = ['sports', 'skateboarding'];
+const activeHobbies = ['hiking', ...hobbies];
+console.log(activeHobbies);
+
+// Spread Operator Objects
+const person = {
+  name: 'tyroo',
+  age: 28
+};
+
+const copiedPerson = {...person};
+console.log(copiedPerson);
