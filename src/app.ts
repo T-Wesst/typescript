c7144446ac5b96ef275cd4dd6f3673d5925158c1
T-Wesst <code.daddy17@gmail.com>
2020-12-15 21:19:10 -0800

    r

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
 // const and let
-const userName = 'Max';
-let age = 28;
-age = 29;
+// const userName = 'Max';
+// let age = 28;
+// age = 29;
 // defaults should be set from right
 // arrow functions
-const add = (a:number, b:number = 1) =>  a + b;
+// const add = (a:number, b:number = 1) =>  a + b;
 // valid TS
-const printOutput: (a:number | string) => void = output => console.log(output);
-const printOutput1 = (output: string | number) => console.log(output);
-printOutput(add(5));
+// const printOutput: (a:number | string) => void = output => console.log(output);
+// const printOutput1 = (output: string | number) => console.log(output);
+// printOutput(add(5));
 
 // Spread Operator Arrays
 const hobbies = ['sports', 'skateboarding'];
@@ -23,3 +23,13 @@ const person = {
 
 const copiedPerson = {...person};
 console.log(copiedPerson);
+
+// Rest Parameters
+const add = (...numbers: number[]) => {
+  return numbers.reduce((currentResult, currentValue) => {
+    return currentResult + currentValue;
+  },0);
+};
+
+const addedNumbers = add(5,4,3,2,1);
+console.log(addedNumbers);
