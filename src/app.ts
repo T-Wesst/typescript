776326a6246081cf05ce168bf8bd8bf2d945a69d
T-Wesst <code.daddy17@gmail.com>
2020-12-15 20:05:31 -0800

    initial commit

--- src/app.ts
+++
@@ -1,8 +0,0 @@
-const username: string = 'Tyroo';
-
-console.log(username);
-
-const button = document.querySelector('button')!;
-button.addEventListener('click', () => {
-  console.log('clicked');
-})
