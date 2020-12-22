cab6beccdc985e92a299467a44c0f2824c7faf68
T-Wesst <code.daddy17@gmail.com>
2020-12-21 22:22:06 -0800

    gitignore fixed

--- dist/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const username = 'Tyroo';
-console.log(username);
-const button = document.querySelector('button');
-button.addEventListener('click', () => {
-    console.log('clicked');
-});
